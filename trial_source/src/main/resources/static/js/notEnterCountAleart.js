2d01614b876faff4a472d5a6cf19ec2bf6f3e618
eri <e_saeki@braingate.co.jp>
2025-08-22 17:49:17 +0900

    fix.task.25製造完了

--- trial_source/src/main/resources/static/js/notEnterCountAleart.js
+++ trial_source/src/main/resources/static/js/notEnterCountAleart.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-	const hasNotEnterCount = document.getElementById('hasNotEnterCount').value;
-	if (hasNotEnterCount) {
-		alert("過去の勤怠に未入力があります。");
-	}
+    // データ属性から値を取得する
+    const hasNotEnterCountElement = document.querySelector('[data-not-enter-count]');
+    const hasNotEnterCount = hasNotEnterCountElement ? hasNotEnterCountElement.dataset.notEnterCount : null;
+    
+    // 値が 'true'（文字列）の場合にアラートを表示
+    if (hasNotEnterCount === 'true') {
+        alert("過去日の勤怠に未入力があります。");
+    }
 });
